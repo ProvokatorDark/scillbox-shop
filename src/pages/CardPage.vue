d96d6ce761c7486d26086c2794cec7a1efceca09
ProvokatorDark <provokatordark@yandex.ru>
2021-07-19 18:56:10 +0500

    Version 6 revision 2

--- src/pages/CardPage.vue
+++ src/pages/CardPage.vue
@@ -18,7 +18,7 @@
         Корзина
       </h1>
       <span class="content__info">
-        {{products.length}} {{howManyItems}}
+        {{$store.getters.cartTotalItems}} {{howManyItems}}
       </span>
     </div>
 
@@ -60,13 +60,13 @@ export default {
   computed:{
     ...mapGetters({products:'cartDetailProducts',totalPrice:'cartTotalPrice'}),
     howManyItems(){
-      let arrLength=this.products.length
+      let arrLength=this.$store.getters.cartTotalItems
       let strLenght = arrLength.toString();
       strLenght=strLenght.split('')
       let last = strLenght[strLenght.length - 1];
       last=Number(last);
       let output='';
-      (arrLength===11)?output='товаров':(last===1)?output='товар':( last > 1 && last<=4)?output='товарa':output='товаров';
+      (arrLength>=11&&arrLength<=20)?output='товаров':(last===1)?output='товар':( last > 1 && last<=4)?output='товарa':output='товаров';
       return output
     }
   },
